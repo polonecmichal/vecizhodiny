3e668e517acc6b680b1a721eeb2e41f8de6467df
Michal <polonecmichal36@gmail.com>
2025-12-17 16:40:44 +0100

    Update ul2.py

--- ul2.py
+++ ul2.py
@@ -1,13 +1,14 @@
-def faktorial(n):
-
-    vysledok = 1
-    for i in range(2, n+1):
-            vysledok *=1
-    return(vysledok)
-pokus = faktorial(5)
-print(pokus)
-
-def combcis(k, n):
-    return  faktorial(n) // (faktorial(k)) * (faktorial(n-k))
-pokus2= combcis(2, 5)
-print(pokus2)
+vstup = input("zadaj mi co idem heslovat:")
+kluc ="abc"
+novy_kluc = kluc * (len(vstup)//len(kluc)) + kluc[:len(vstup)%len(kluc):]
+print(len(novy_kluc)), len(vstup)
+vystup = " "
+for i in range(len(vstup)):
+    znak = chr(ord(vstup[i]))
+    if 97 <= ord(vstup[i]) and ord(vstup[i]) <= 122 :
+        posun = ord(novy_kluc[i])-96
+        ord(vstup[i]) - 97 + posun % 26 + 97
+        vystup += chr((ord(vstup[i]) - 97 + posun) % 26 + 97)
+    else:
+        vystup += vstup[i]
+print(vystup)
